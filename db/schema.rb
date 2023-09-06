4d9c0cd73c86c7182086382cbf30f99c81dfa22b
jmilljr24 <jmilljr24@gmail.com>
2023-09-06 17:33:53 -0400

    Add airport model with arrival and departing associations

--- db/schema.rb
+++ db/schema.rb
@@ -10,11 +10,25 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[7.0].define(version: 2023_09_06_194422) do
+ActiveRecord::Schema[7.0].define(version: 2023_09_06_203812) do
   create_table "airports", force: :cascade do |t|
     t.string "code"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
+    t.string "city"
   end
 
+  create_table "flights", force: :cascade do |t|
+    t.datetime "departure"
+    t.integer "duration"
+    t.integer "departure_airport_id", null: false
+    t.integer "arrival_airport_id", null: false
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+    t.index ["arrival_airport_id"], name: "index_flights_on_arrival_airport_id"
+    t.index ["departure_airport_id"], name: "index_flights_on_departure_airport_id"
+  end
+
+  add_foreign_key "flights", "airports", column: "arrival_airport_id"
+  add_foreign_key "flights", "airports", column: "departure_airport_id"
 end
--- db/seeds.rb
+++ db/seeds.rb
@@ -6,15 +6,50 @@
 #   movies = Movie.create([{ name: "Star Wars" }, { name: "Lord of the Rings" }])
 #   Character.create(name: "Luke", movie: movies.first)
 Airport.destroy_all
+Flight.destroy_all
+airport_list = ['NRT', 'Tokyo'],
+               ['PKX', 'Beijing'],
+               ['JFK', 'New York'],
+               ['LAX', 'Los Angeles'],
+               ['YYZ', 'Toronto'],
+               ['LHR', 'London'],
+               ['CDG', 'Paris'],
+               ['SYD', 'Sydney'],
+               ['ICN', 'Seoul']
 
-Airport.create!([{
-                  code: 'KOIC'
-                },
-                 {
-                   code: 'KBGM'
-                 },
-                 {
-                   code: 'KALB'
-                 }])
+airport_list.each do |code, city|
+  Airport.create(code:, city:)
+end
 
-p "Created #{Airport.count} airports"
+# Flight Model
+
+dates = (Date.today..(Date.today + 1.week)).to_a
+airports = Airport.all
+
+# first = Airport.first
+# last = Airport.last
+# length = rand(90..300)
+# Flight.create(
+#   departure: DateTime.now,
+#   duration: length,
+#   departure_airport_id: first,
+#   arrival_airport_id: last
+# )
+
+airports.each do |depart|
+  airports.each do |arrive|
+    next if depart == arrive
+
+    length = rand(90..300)
+    dates.each do |d|
+      rand(1..3).times do
+        Flight.create(
+          departure: DateTime.new(d.year, d.month, d.day, rand(6..23), rand(0..59)),
+          duration: length,
+          departure_airport_id: depart.id,
+          arrival_airport_id: arrive.id
+        )
+      end
+    end
+  end
+end

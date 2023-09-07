8d640365e4355f16a985e9f2840c7f50050482d8
jmilljr24 <jmilljr24@gmail.com>
2023-09-07 16:09:16 -0400

    Fix routes for _results partial. Add pass number to search

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,8 @@
 Rails.application.routes.draw do
-  get 'airports/index'
+  # get 'airports/index'
+
+  resources :flights
+
   root 'flights#index'
   # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
 

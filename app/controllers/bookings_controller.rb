5c1b06770fe3e249b2baf7d1098717a6f8eb0a01
jmilljr24 <jmilljr24@gmail.com>
2023-09-08 12:08:05 -0400

    Add subform for multiple passengers

--- app/controllers/bookings_controller.rb
+++ app/controllers/bookings_controller.rb
@@ -1,15 +1,19 @@
 class BookingsController < ApplicationController
   def new
-    @booking = Booking.new
-    @flight = Flight.find(params[:flight_id])
+    @booking = Booking.new(flight_id: params[:flight_id])
     @num_passengers = params[:num_passengers]
-    @booking.passengers.build
+    # @num_passengers.times { @booking.passengers.build }
+    @passengers = @booking.passengers.build
+    @flight = Flight.find(params[:flight_id])
   end
 
   def create
-    @flight = Flight.find(params[:flight_id])
-    @booking = @flight.bookings.build(booking_params)
-
+    # @flight = Flight.find(params[:flight_id])
+    # @booking = @flight.bookings.build(booking_params)
+    @booking = Booking.create(booking_params)
+    # @booking.flight = @flight
+    # @passengers = @booking.passengers
+    # @booking = Booking.new(booking_params)
     if @booking.save
       redirect_to @booking
     else
